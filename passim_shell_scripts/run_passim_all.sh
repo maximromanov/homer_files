14ab2ecdf0fb46398a4d8170a3fd637945af19c4
Maxim Romanov <romanov.maxim@gmail.com>
2018-03-31 20:57:00 +0100

    passim shell script with postprocessing

--- passim_shell_scripts/run_passim_all.sh
+++ passim_shell_scripts/run_passim_all.sh
@@ -2,3 +2,4 @@
 
 SPARK_SUBMIT_ARGS='--master local[6] --driver-memory 50G --executor-memory 50G --conf spark.local.dir=/home/mromanov/passim_runs/tmp --conf spark.sql.shuffle.partitions=1000' passim --pairwise --output-format parquet --filterpairs 'gid != gid2' --n 4 --min-match 4 input output >& run.err
 
+spark-submit $SPARK_SUBMIT_ARGS pair-partition.py output/align.parquet output/align-partition-pair >& script-pair.err
